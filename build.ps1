e19f9ff6d0570d516c7521e1121b88af61b580bf
Livar Cunha <livar@live.com>
2017-03-27 09:31:09 -0700

    Adding CI to xliff-tasks.

---
+++ build.ps1
@@ -0,0 +1,28 @@
+[cmdletbinding()]
+param(
+    [Parameter(Position=0, ValueFromRemainingArguments=$true)]
+    $ExtraParameters
+)
+$ErrorActionPreference="Stop"
+$ProgressPreference="SilentlyContinue"
+
+$RepoRoot = "$PSScriptRoot"
+$DOTNET_INSTALL_DIR="$REPOROOT/.dotnet"
+
+$env:XDG_DATA_HOME="$REPOROOT/.nuget/packages"
+$env:NUGET_PACKAGES="$REPOROOT/.nuget/packages"
+$env:NUGET_HTTP_CACHE_PATH="$REPOROOT/.nuget/packages"
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
+if (-Not (Test-Path $DOTNET_INSTALL_DIR))
+{
+    New-Item -Type "directory" -Path $DOTNET_INSTALL_DIR 
+}
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile "$DOTNET_INSTALL_DIR/dotnet-install.ps1"
+& $DOTNET_INSTALL_DIR/dotnet-install.ps1 -Version "1.0.1" -InstallDir "$DOTNET_INSTALL_DIR"
+
+$env:PATH="$DOTNET_INSTALL_DIR;$env:PATH"
+
+& dotnet msbuild build.proj /t:MakeVersionProps
+& dotnet msbuild build.proj /v:diag /fl /flp:v=diag $ExtraParameters
---
+++ build.sh
@@ -0,0 +1,44 @@
+set -e
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+REPOROOT="$DIR"
+
+# Some things depend on HOME and it may not be set. We should fix those things, but until then, we just patch a value in
+if [ -z "$HOME" ]; then
+    export HOME="$DIR/.home"
+
+    [ ! -d "$HOME" ] || rm -Rf $HOME
+    mkdir -p $HOME
+fi
+
+# $args array may have empty elements in it.
+# The easiest way to remove them is to cast to string and back to array.
+# This will actually break quoted arguments, arguments like 
+# -test "hello world" will be broken into three arguments instead of two, as it should.
+args=( "$@" )
+temp="${args[@]}"
+args=($temp)
+
+export XDG_DATA_HOME="$REPOROOT/.nuget/packages"
+export NUGET_PACKAGES="$REPOROOT/.nuget/packages"
+export NUGET_HTTP_CACHE_PATH="$REPOROOT/.nuget/packages"
+export DOTNET_INSTALL_DIR="$REPOROOT/.dotnet"
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
+if [ ! -d "$DOTNET_INSTALL_DIR" ]; then
+  mkdir $DOTNET_INSTALL_DIR
+fi
+
+curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --install-dir $DOTNET_INSTALL_DIR --version 1.0.1
+
+PATH="$DOTNET_INSTALL_DIR:$PATH"
+
+dotnet msbuild build.proj /t:MakeVersionProps
+dotnet msbuild build.proj /v:diag /fl /flp:v=diag "${args[@]}"
